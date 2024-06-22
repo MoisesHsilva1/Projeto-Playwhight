c18a38ecab29621c16c5489676fcf3cf22dba732
Moises Henrique <moihgm67@gmail.com>
2024-06-21 23:05:36 -0300

    Atualizando Projeto

--- tests/TesteFailLogin.spec.js
+++ tests/TesteFailLogin.spec.js
@@ -1,8 +1,28 @@
 import {test, expect } from '@playwright/test'
 
 
-test('Teste',  async ({ page }) => {
+test('Teste visualizando as mensagem de obrigatoridade dos campos',  async ({ page }) => {
     await page.goto('https://parabank.parasoft.com/parabank/lookup.htm')
 
-    await page.click('.button');
+    const mensagemFailName =  'First name is required.';
+    const mensagemFailLastName = 'Last name is required.';
+    const mensagemAddressFail  = 'Address is required.'
+    const mensagemCityFail =  'span'
+   
+    const buttonFail =  'input.button'
+
+    await page.click(buttonFail)
+
+    await page.getByRole('span', {mensagemFailName}).isVisible();
+    await page.getByRole('span', {mensagemFailLastName}).isVisible();
+    await page.getByRole('span', {mensagemAddressFail}).isVisible();
+    await page.getByRole(mensagemCityFail, { name: 'City is required.'}).isVisible();
+
+
+    if(mensagemCityFail) {
+        console.log('mensagem de falha esta visivel')
+    } else {
+        console.log('A mensagem não esta visivel')
+    }
+
 });
