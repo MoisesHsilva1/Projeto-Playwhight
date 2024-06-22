91a6322869ce0d5b2ac5d10f99932c836b189d4e
Moises Henrique <moihgm67@gmail.com>
2024-06-21 22:36:28 -0300

    Atualizando Projeto

--- tests/TesteLogin.spec.js
+++ tests/TesteLogin.spec.js
@@ -10,13 +10,16 @@ test('Realizando Login Correto', async ({ page }) => {
      
     await page.fill('#firstName', 'Teste Name');
 
-    await page.fill('#lastname', 'Last Name test');
+    await page.fill('#lastName', 'Last Name test');
 
-    await page.fill('#address.street', 'Rua da casa do QA');
+    await page.fill('#address\\.street', 'Rua da casa do QA');
 
-    await page.fill('#address.city', 'Cidade do QA')
+    await page.fill('#address\\.city', 'Cidade do QA')
 
-    await page.fill('#address.state', 'São Paulo')
+    await page.fill('#address\\.state', 'São Paulo')
 
-    await page.fill('')
+    await page.fill('#address\\.zipCode', '086454120')
+    
+    await page.fill('#ssn', '036-56-0519')
+    
 })
