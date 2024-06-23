4f8d9fe12c26d1e05e1db4b776c9407e8462e090
Moises Henrique <moihgm67@gmail.com>
2024-06-22 22:33:46 -0300

    Atualizando Projeto

--- tests/TesteVerificarCampos.spec.js
+++ tests/TesteVerificarCampos.spec.js
@@ -2,10 +2,17 @@ import { test } from "@playwright/test";
 
 
 test('Teste visualizando as mensagem de obrigatoridade dos campos',  async ({ page }) => {
-     const url = "https://parabank.parasoft.com/parabank/lookup.htm"
-     const FistName = 'input#firstName'
+    const url = "https://parabank.parasoft.com/parabank/lookup.htm";
+    const FistName = 'input#firstName';
+    const LastName =  'input#lastName';
+    const Addres = 'input#addres//street';
 
     await page.goto(url)
 
     await page.locator(FistName).waitFor({ state: 'visible' });
+    await page.locator(LastName).waitFor({state: "visible"});
+    await page.locator(Addres).waitFor({state: visible});
+    
+
+
 })
