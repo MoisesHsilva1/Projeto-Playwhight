32ed0579a77fd3e226c5f3052dec763a229e756a
Moises Henrique <moihgm67@gmail.com>
2024-06-22 22:23:09 -0300

    Atualizando Projeto

--- tests/TesteVerificarCampos.spec.js
+++ tests/TesteVerificarCampos.spec.js
@@ -1,1 +1,13 @@
-import { expect } from "@playwright/test";
+import { test } from "@playwright/test";
+
+
+test('Teste visualizando as mensagem de obrigatoridade dos campos',  async ({ page }) => {
+     const url = "https://parabank.parasoft.com/parabank/lookup.htm"
+     const FieldsRegisterUser = {
+        "FistName": "firstName"
+     }; 
+
+    await page.goto(url)
+
+    await page.isVisible(FistName)
+})
